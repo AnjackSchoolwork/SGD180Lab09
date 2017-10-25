0d079db0127255ac361e4187140b34829f48f3a9
Anjack <gary.reif@gmail.com>
2017-10-24 21:52:24 -0400

    Updated animation code. It doesn't work.

--- Lab09_Anim.js
+++ Lab09_Anim.js
@@ -2,6 +2,9 @@
 function Setup() {
 	game = new Scene()
 
+	background = new Sprite(game, "background.jpg", 800, 600)
+	background.setPosition(400, 300)
+	background.setSpeed(0)
 	thePlayer = new Player(game, "rpg_sprite_walk.png", 192, 128, 100, 100)
 
 	game.start()
@@ -9,5 +12,6 @@ function Setup() {
 
 function update() {
 	game.clear()
+	background.update()
 	thePlayer.Update()
 }
--- Lab09_Anim_Classes.js
+++ Lab09_Anim_Classes.js
@@ -20,45 +20,74 @@ class Player extends Entity {
 		this.sprite.loadAnimation(192, 128, 24, 32)
 		this.sprite.generateAnimationCycles()
 		this.sprite.renameCycles(new Array("down", "up", "left", "right"))
-		this.sprite.setAnimationSpeed(500)
+		this.sprite.setAnimationSpeed(300)
 
 		this.sprite.pauseAnimation()
 		this.sprite.setCurrentCycle("down")
+
+		this.y_mov = 0
+		this.x_mov = 0
 	}
 
 	Update() {
 		super.Update()
-		if (keysDown[K_LEFT]) {
-			this.sprite.setSpeed(1);
-			this.sprite.playAnimation()
-			this.sprite.setMoveAngle(270);
-			this.sprite.setCurrentCycle("left");
+
+		this.sprite.setSpeed(5)
+		this.sprite.playAnimation()
+		
+		this.setMoveState()
+
+		if (this.x_mov < 0) {
+			this.sprite.setMoveAngle(270)
+			this.sprite.setCurrentCycle("left")
 		}
-		if (keysDown[K_RIGHT]) {
-			this.sprite.setSpeed(1);
-			this.sprite.playAnimation()
-			this.sprite.setMoveAngle(90);
-			this.sprite.setCurrentCycle("right");
+		else if (this.x_mov > 0) {
+			this.sprite.setMoveAngle(90)
+			this.sprite.setCurrentCycle("right")
 		}
-		if (keysDown[K_UP]) {
-			this.sprite.setSpeed(1);
-			this.sprite.playAnimation()
-			this.sprite.setMoveAngle(0);
-			this.sprite.setCurrentCycle("up");
+		else if (this.y_mov < 0) {
+			this.sprite.setMoveAngle(0)
+			this.sprite.setCurrentCycle("up")
 		}
-		if (keysDown[K_DOWN]) {
-			this.sprite.setSpeed(1);
-			this.sprite.playAnimation()
-			this.sprite.setMoveAngle(180);
-			this.sprite.setCurrentCycle("down");
+		else if (this.y_mov > 0) {
+			this.sprite.setMoveAngle(180)
+			this.sprite.setCurrentCycle("down")
+		}
+		else {
+			this.sprite.setSpeed(0)
+			this.sprite.pauseAnimation()
 		}
 
-		if (keysDown[K_SPACE]) {
-			this.sprite.setSpeed(0);
-			this.sprite.pauseAnimation();
-			this.sprite.setCurrentCycle("down");
-		}
 		this.sprite.update()
+	}
+
+	setMoveState() {
+
+		this.x_mov = 0
+		this.y_mov = 0
+
+
+		if (keysDown[K_LEFT]) {
+			if (this.x_mov > -1) {
+				this.x_mov--
+			}
+		}
+		if (keysDown[K_RIGHT]) {
+			if (this.x_mov < 1) {
+				this.x_mov++
+			}
+		}
+		if (keysDown[K_UP]) {
+			if (this.y_mov > -1) {
+				this.y_mov--
+			}
+		}
+		if (keysDown[K_DOWN]) {
+			if (this.y_mov < 1) {
+				this.y_mov++
+			}
+		}
+
 	}
 
 }
